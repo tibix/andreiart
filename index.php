82ac01c56c7aec0ca00d18598ff3104010db3fe6
Tiberiu Dascal <tiberiu.purcel@gmail.com>
2016-06-07 13:12:59 +0300

    linked bootsrap framework! tested proper functionality with h1 tag in index.php

--- index.php
+++ index.php
@@ -3,11 +3,12 @@
     <head>
         <title>Andrei Art::Home made art</title>
         <link rel="stylesheet" type="text/css" href="main.css" />
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css" />
     </head>
     <body>
         <div class="main">
             <div class="content">
-                
+                <h1>Working properly! Move furhter!</h1>
             </div>
         </div>
     <script type='text/javascript' src='app.js'></script>
