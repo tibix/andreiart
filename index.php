cc53764f10cafdeebc53ee6e6302a33370a644bc
Tiberiu Dascal <tiberiu.purcel@gmail.com>
2016-06-07 09:24:22 +0300

    Adding initial skeletal and internal links: index.php includes main.css and app.js

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Andrei Art::Home made art</title>
+        <link rel="stylesheet" type="text/css" href="main.css" />
+    </head>
+    <body>
+    
+
+    <script type='text/javascript' src='app.js'></script>
+    </body>
+</html>
+<?php
+
+?>
